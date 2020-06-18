d0dc4587b6100cbb60d937696c5654794115bdc7
sarahveldhuis <sarahmveldhuis@gmail.com>
2020-06-18 15:46:30 -0300

    alterado pacotes de urls e adicionado o ChatInfoCommand

---
+++ app/src/main/java/com/example/thecomputersmm/Command/ChatInfoCommand.java
@@ -0,0 +1,48 @@
+package com.example.thecomputersmm.Command;
+
+public class ChatInfoCommand {
+    private Integer userId;
+    private String username;
+    private Integer roomId;
+    private String roomName;
+
+    public ChatInfoCommand(Integer userId, Integer roomId, String username, String roomName) {
+        this.userId = userId;
+        this.roomId = roomId;
+        this.username = username;
+        this.roomName = roomName;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Integer roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+}
+
--- app/src/main/java/com/example/thecomputersmm/Url.java
+++ app/src/main/java/com/example/thecomputersmm/Url.java
@@ -1,21 +1,22 @@
 package com.example.thecomputersmm;
 
 public class Url {
-    static public String base = "http://192.168.0.16:8080/";
-    //Sarah:"http://192.168.1.6:8080/"
-    //Milena: "http://192.168.15.10:8080/"
+    static public String ip = "192.168.1.6:8080/";
+    //Sarah:"192.168.1.6:8080/"
+    //Milena: "192.168.15.10:8080/"
 
-    static public String login = base + "login";
-    static public String newUser = base + "newUser";
-    static public String getUsers = base + "getUsers";
-    static public String createRoom = base + "createRoom";
-    static public String logout = base + "logout";
-    static public String getUserRooms = base + "getUserRooms";
-    static public String getLastMessage = base + "getLastMessage";
-    static public String addUserToRoom = base + "addUserToRoom";
-    static public String updateUserPassword = base + "updateUserPassword";
-    static public String deleteUser = base + "removeUser";
-    static public String getMessages = base + "getMessages";
-
-
+    static public String login = "http://" + ip + "login";
+    static public String newUser = "http://" + ip + "newUser";
+    static public String getUsers = "http://" + ip + "getUsers";
+    static public String createRoom = "http://" + ip + "createRoom";
+    static public String logout = "http://" + ip + "logout";
+    static public String getUserRooms = "http://" + ip + "getUserRooms";
+    static public String getLastMessage = "http://" + ip + "getLastMessage";
+    static public String addUserToRoom = "http://" + ip + "addUserToRoom";
+    static public String updateUserPassword = "http://" + ip + "updateUserPassword";
+    static public String deleteUser = "http://" + ip + "removeUser";
+    static public String getMessages = "http://" + ip + "getMessages";
+    static public String chatInfo = "http://" + ip + "chatInfo";
+    static public String webSocket = "ws://" + ip + "mywebsockets/websocket";
 }
+
